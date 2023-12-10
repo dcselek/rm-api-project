ecba346e34e3c6cf4a8a694a20b6715f3bad973c
dcselek <canselek2434@gmail.com>
2023-12-10 23:28:18 +0300

    Add LocationCard component

---
+++ src/components/LocationCard/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./locationCard.module.scss";
+import Link from "next/link";
+
+interface Location {
+  id: number;
+  name: string;
+  type: string;
+  dimension: string;
+  residents: string[];
+  url: string;
+  created: string;
+}
+
+interface LocationCardProps {
+  location: Location;
+}
+
+const LocationCard: React.FC<LocationCardProps> = ({ location }) => {
+  const { name, type, dimension, residents, id } = location;
+  const residentsCount = residents.length;
+
+  return (
+    <div className={styles["location-card"]}>
+      <Link className={styles.link} href={`locations/${id}`}>{name}</Link>
+      <p>
+        <span>Type:</span> {type}
+      </p>
+      <p>
+        <span>Dimension:</span> {dimension}
+      </p>
+      <p>
+        <span>Residents Count:</span> {residentsCount}
+      </p>
+    </div>
+  );
+};
+
+export default LocationCard;
