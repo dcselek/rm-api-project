b54289a04cbdba5d7173f7ae8dbfc178476332e8
dcselek <canselek2434@gmail.com>
2023-12-11 21:03:02 +0300

    Refactor location page styles and add filter buttons

--- src/app/locations/[id]/index.tsx
+++ src/app/locations/[id]/index.tsx
@@ -3,7 +3,9 @@ import CharacterCard from "@/components/CharacterCard";
 import axiosInstance from "@/lib/axios";
 import { useMutation } from "@tanstack/react-query";
 import React from "react";
-import "./characters.scss"
+import "./characters.scss";
+import BackButton from "@/components/BackButton";
+import FilterButton from "@/components/FilterButton";
 
 interface Location {
   params: {
@@ -35,11 +37,18 @@ interface CharacterType {
 const LocationPage: React.FC<Location> = ({ params }) => {
   const [dataLocation, setDataLocation] = React.useState<any>(null);
   const [characters, setCharacters] = React.useState<CharacterType[]>([]);
+  const [filter, setFilter] = React.useState<
+    "all" | "alive" | "dead" | "unknown"
+  >("all");
+  const [filteredCharacters, setFilteredCharacters] = React.useState<
+    CharacterType[]
+  >([]);
 
   const getCharacters = useMutation({
     mutationFn: (residents) => axiosInstance.get(`/character/${residents}`),
     onSuccess: ({ data }) => {
       setCharacters(data);
+      setFilteredCharacters(data);
     },
   });
 
@@ -58,22 +67,49 @@ const LocationPage: React.FC<Location> = ({ params }) => {
     getLocation.mutate(params.id);
   }, []);
 
+  React.useEffect(() => {
+    if (characters.length > 0) {
+      if (filter === "all") {
+        setFilteredCharacters(characters);
+      } else {
+        const filtered = characters.filter((item) => item.status.toLowerCase() === filter);
+        setFilteredCharacters(filtered);
+      }
+    }
+  }, [filter]);
+
   return (
     <div>
+      <BackButton />
       {dataLocation && (
-        <div>
+        <div className="data-location-card">
           <h1>{dataLocation.name}</h1>
           <p>{dataLocation.type}</p>
           <p>{dataLocation.dimension}</p>
           <p>{dataLocation.created}</p>
         </div>
       )}
-      <div className="characters-list">
-      {characters &&
-        characters.map((item: CharacterType) => (
-          <CharacterCard character={item} key={item.id} />
-        ))}
+      <div className="filter-buttons">
+        <div className="filter-buttons">
+          {["all", "alive", "dead", "unknown"].map((item) => (
+            <FilterButton
+              filter={filter}
+              setFilter={() => {
+                setFilter(item as "unknown" | "all" | "alive" | "dead");
+              }}
+              key={item}
+            >
+              {item}
+            </FilterButton>
+          ))}
         </div>
+      </div>
+      <div className="characters-list">
+        {filteredCharacters &&
+          filteredCharacters.map((item: CharacterType) => (
+            <CharacterCard character={item} key={item.id} />
+          ))}
+      </div>
     </div>
   );
 };
